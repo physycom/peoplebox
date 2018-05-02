86382c0ba8cdb5520f4b1fdc5750d31740818b52
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-05-02 10:21:10 +0200

    windows enhancements

--- build.cmd
+++ build.cmd
@@ -3,7 +3,6 @@ call "%PROGRAMFILES(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Bu
 rmdir /S /Q build
 mkdir build
 cd build
-REM cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=%WORKSPACE%\vcpkg_opencv2\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=%VCPKG_DEFAULT_TRIPLET%" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=%WORKSPACE%\vcpkg_opencv2\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=%VCPKG_DEFAULT_TRIPLET%" "-DCMAKE_BUILD_TYPE=Debug" ..
-cmake --build . --config Release
+cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=%WORKSPACE%\vcpkg_opencv2\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=%VCPKG_DEFAULT_TRIPLET%" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake --build . --config Release --target install
 cd ..
--- build.ps1
+++ build.ps1
@@ -5,6 +5,6 @@ New-Item -Path .\build -ItemType directory -Force
 Set-Location build
 
 cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg_opencv2\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build . --config Release
+cmake --build . --config Release --target install
 
 Set-Location ..
