ef440f468a6503e36ae502fdad1fdfedc6819cdc
MrRomanMaccaruni <rommacc@yahoo.it>
2018-05-03 18:56:42 +0200

    a little safety goes a long way

--- src/darktrack.c
+++ src/darktrack.c
@@ -27,10 +27,11 @@ double demo_time;
 
 // physycom add
 #define MAX_FRAME_INFO_TO_STORE 50
-#define FRAME_NUMBER_TRACKING 5
-#define MAX_LINE_LEN 20
-#define ERR_NO_FILE 111
-#define ERR_NO_STREAM 222
+#define FRAME_NUMBER_TRACKING   5
+#define MAX_LINE_LEN            20
+#define ERR_NO_FILE             111
+#define ERR_NO_STREAM           222
+#define ERR_NO_INFO_JSON        333
 
 #define PUNCTILIOUS
 
@@ -126,6 +127,10 @@ void* detect_in_thread(void* ptr)
   if (frame_num % MAX_FRAME_INFO_TO_STORE == 0) {
     sprintf(json_name, "output/frame_info.%ld.json", tnow_t);
     FILE* info_json = fopen(json_name, "w");
+    if(!info_json){
+      fprintf(stderr, "Cannot create info json\n");
+      exit(ERR_NO_INFO_JSON);
+    }
     fprintf(info_json, "{\n");
     for (i = 0; i < MAX_FRAME_INFO_TO_STORE; ++i) {
       fprintf(info_json, "\t\"frame_%d\" : {\n", infos[i].frame_number);
