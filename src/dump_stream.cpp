dc88ee36d676c6421e56dcc34f3fabb397c73f48
MrRomanMaccaruni <rommacc@yahoo.it>
2018-05-03 11:10:54 +0200

    fake push

--- src/dump_stream.cpp
+++ src/dump_stream.cpp
@@ -18,6 +18,7 @@ int main(int argc, char** argv)
   int frame_width   = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
   int frame_height  = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
   cv::VideoWriter video("out.avi", CV_FOURCC('M','J','P','G'), 10, cv::Size(frame_width,frame_height), true);
+
   for(;;)
   {
     cv::Mat frame;
@@ -27,5 +28,6 @@ int main(int argc, char** argv)
     char c = (char)cv::waitKey(33);
     if( c == 27 ) break;
   }
+
   return 0;
 }
