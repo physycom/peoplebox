a2085c323dbb34e4273aebfa3c2531faea723f04
MrRomanMaccaruni <rommacc@yahoo.it>
2018-05-15 12:44:34 +0200

    add deploy target, fix save_frame bug

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
- export CC="/usr/local/bin/gcc-7"
- export CXX="/usr/local/bin/g++-7"
+  export CC="/usr/local/bin/gcc-7"
+  export CXX="/usr/local/bin/g++-7"
+  remote="$2"
+else
+  remote="$1"
 fi
 
 mkdir -p build
@@ -10,3 +13,10 @@ cd build
 cmake ..
 cmake --build . --target install
 cd ..
+
+if [[ $remote != "" ]]; then
+  cd build
+  cmake -D remote=$remote ..
+  cmake --build . --target deploy
+  cd ..
+fi
