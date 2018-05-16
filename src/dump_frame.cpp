ff8b82cabca96a176a57e2a43f68212a448730ec
MrRomanMaccaruni <rommacc@yahoo.it>
2018-05-16 15:51:10 +0200

    update with fixes

--- src/dump_frame.cpp
+++ src/dump_frame.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <ctime>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <thread>
 
@@ -16,16 +17,16 @@ int main(int argc, char** argv)
   int frame_to_dump;
   if (argc > 2)
   {
-    input = argv[1];
     try
     {
-      frame_to_dump = std::stoi(argv[2]);
+      frame_to_dump = std::stoi(argv[1]);
     }
     catch(std::exception &e)
     {
       std::cerr << "Error in command line : " << e.what() << std::endl;
       exit(ERROR_CLI);
     }
+    input = argv[2];
   }
   else{
     std::cerr << "Usage : " << argv[0] << " number_of_frames_to_dump path/to/video" << std::endl;
@@ -43,13 +44,15 @@ int main(int argc, char** argv)
   int frame_cnt = 0;
   for (int i = 0; i < frame_to_dump; ++i) {
     vcap >> frame;
-    frame_name = std::string(IMAGE_FOLDER) + "/frame_" + std::to_string(i) + ".jpg";
-    if (imwrite(frame_name + ".jpg", frame))
-      std::cout << "Frame " << frame_cnt << " (" << frame_name << ") dumped successfully" << std::endl;
+    std::stringstream ss;
+    ss << IMAGE_FOLDER << "/frame_" << std::setw(5) << std::setfill('0') << i << ".jpg";
+    if (imwrite(ss.str(), frame))
+      std::cout << "Frame " << frame_cnt << " (" << ss.str() << ") dumped successfully" << std::endl;
     else {
       std::cerr << "Unable to write frame" << std::endl;
       exit(ERROR_DUMP_FRAME);
     }
+    ++frame_cnt;
   }
   vcap.release();
 
