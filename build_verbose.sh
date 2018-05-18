df25b6891027c57ac1e160ee98975a6ff12674dc
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-05-18 19:56:23 +0200

    add verbose

---
+++ build_verbose.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
+  export CC="/usr/local/bin/gcc-7"
+  export CXX="/usr/local/bin/g++-7"
+  remote="$2"
+else
+  remote="$1"
+fi
+
+mkdir -p build
+cd build
+cmake .. -DVERBOSE:BOOL=TRUE
+cmake --build . --target install
+cd ..
+
