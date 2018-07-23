8de1d179789d4ff204ca3d7b6a8124b6bc2d10be
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-07-23 23:25:15 +0200

    prepare to analyse scan

--- script/json_counter.py
+++ script/json_counter.py
@@ -11,60 +11,89 @@ times = {}
 dates = {}
 count = {}
 cumulate = {}
+scaling_factor = {}
+fitted = {}
+
+id_in='BETA_CNAF-IN'
+id_out='BETA_CNAF-OUT'
 
 #path = os.environ['WORKSPACE'] + os.path.sep + 'peoplebox' + os.path.sep + 'data' + os.path.sep + 'barriera_900'
 #requires_fit=True
+#requires_norm=False
 
-path = os.environ['WORKSPACE'] + os.path.sep + 'peoplebox' + os.path.sep + 'data' + os.path.sep + 'newfit'
-requires_fit=False
+#path = os.environ['WORKSPACE'] + os.path.sep + 'peoplebox' + os.path.sep + 'data' + os.path.sep + 'newfit'
+#requires_fit=False
+#requires_norm=False
 
-tags=[
-'20180716_1200',
-'20180716_1600',
-'20180716_2000',
-'20180717_0800',
-'20180717_1200',
-#'20180717_1200_1',
-#'20180717_1200_2',
-'20180719_2000',
-'20180720_0800']
+path = os.environ['WORKSPACE'] + os.path.sep + 'peoplebox' + os.path.sep + 'data' + os.path.sep + 'scan'
+requires_fit=True
+requires_norm=True
+
+barriers=['700_700',
+          '750_750',
+          '800_800',
+          '850_850',
+          '900_900',
+          '950_950',
+          '1000_1000',
+          '1050_1050',
+          '1100_1100',
+          '1150_1150',
+          '1200_1200'
+          ]
+
+tags=['20180716-1200',
+      '20180716-1600',
+#      '20180716-2000',
+      '20180717-0800',
+      '20180717-1200',
+      #'20180717-1200-1',
+      #'20180717-1200-2',
+      '20180719-2000',
+      '20180720-0800',
+#      '20180720-1200'
+      ]
 
 gt = {
-      '20180716_1200' : {
+      '20180716-1200' : {
               'BETA_CNAF-IN'  : 312,
               'BETA_CNAF-OUT' : 169
               },
-      '20180716_1600' : {
+      '20180716-1600' : {
               'BETA_CNAF-IN'  : 107,
               'BETA_CNAF-OUT' : 377
               },
-      '20180716_2000' : {
+      '20180716-2000' : {
               'BETA_CNAF-IN'  : 96,
               'BETA_CNAF-OUT' : 154
               },
-      '20180717_0800' : {
+      '20180717-0800' : {
               'BETA_CNAF-IN'  : 271,
               'BETA_CNAF-OUT' : 107
               },
-      '20180717_1200' : {
+      '20180717-1200' : {
               'BETA_CNAF-IN'  : 292,
               'BETA_CNAF-OUT' : 179
               },
-      '20180717_1200_1' : {
+      '20180717-1200-1' : {
               'BETA_CNAF-IN'  : 143,
               'BETA_CNAF-OUT' : 103
               },
-      '20180717_1200_2' : {
+      '20180717-1200-2' : {
               'BETA_CNAF-IN'  : 130,
               'BETA_CNAF-OUT' : 67
               },
-      '20180719_2000' : {
+      '20180719-2000' : {
               'BETA_CNAF-IN'  : 207,
               'BETA_CNAF-OUT' : 237
               },
-      '20180720_0800' : {
+      '20180720-0800' : {
               'BETA_CNAF-IN'  : 300,
               'BETA_CNAF-OUT' : 104
+              },
+      '20180720-1200' : {
+              'BETA_CNAF-IN'  : 212,  #pepe 228
+              'BETA_CNAF-OUT' : 144   #pepe 157
               }
       }
 
@@ -86,8 +115,10 @@ gt = {
 #timestamp_max=1531808100
 
 #%%
-prob_in = 1.25
-prob_out = 0.99
+#prob_in = 1.25
+#prob_out = 0.99
+prob_in = 0.944
+prob_out = 0.9089
 np.random.seed(12345)
 def t(n, prob):
     nt = n
@@ -97,43 +128,79 @@ def t(n, prob):
         nt += int(prob)
     return nt
 
+def get_sf(dic):
+    sf = {}
+    sf[id_in] = 0.0
+    sf[id_out] = 0.0
+    for tag in tags:
+        sf[id_in] += dic[tag][id_in]
+        sf[id_out] += dic[tag][id_out]
+    sf[id_in] /= len(tags)
+    sf[id_out] /= len(tags)
+    return sf
+
+
 #%%
 
-for tag in tags:
-    count[tag] = {}
-    cumulate[tag] = {}
-    times[tag] = {}
-    dates[tag] = {}
-    files=os.listdir(path + os.path.sep + tag)
-    for file in files:
-        timestamp=int(file.split(os.path.sep)[-1].split('.')[0].split('_')[-1])
-        #if timestamp < timestamp_min or timestamp > timestamp_max:
-            #continue
-
-        with open(path + os.path.sep + tag + os.path.sep + file) as f:
-            data = json.load(f, object_pairs_hook=OrderedDict)
-
-        for key in data:
-            if not count[tag]:
-                count[tag][data[key]["people_count"][0]["id"]] = []
-                count[tag][data[key]["people_count"][1]["id"]] = []
-                times[tag] = []
-                dates[tag] = []
-            times[tag].append(data[key]["timestamp"])
-            dates[tag].append(datetime.datetime.fromtimestamp(int(data[key]["timestamp"])).strftime('%H:%M:%S'))
-            if requires_fit:
-                count[tag][data[key]["people_count"][0]["id"]].append(t(data[key]["people_count"][0]["count"], prob_in))
-                count[tag][data[key]["people_count"][1]["id"]].append(t(data[key]["people_count"][1]["count"], prob_out))
+for barrier in barriers:
+    count[barrier] = {}
+    cumulate[barrier] = {}
+    times[barrier] = {}
+    dates[barrier] = {}
+    scaling_factor[barrier] = {}
+    for tag in tags:
+        count[barrier][tag] = {}
+        cumulate[barrier][tag] = {}
+        times[barrier][tag] = {}
+        dates[barrier][tag] = {}
+        scaling_factor[barrier][tag] = {}
+        folder=path + os.path.sep + tag + '_' + barrier
+        files=os.listdir(folder)
+        for file in files:
+            timestamp=int(file.split(os.path.sep)[-1].split('.')[0].split('_')[-1])
+            #if timestamp < timestamp_min or timestamp > timestamp_max:
+                #continue
+    
+            with open(folder + os.path.sep + file) as f:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+    
+            for key in data:
+                if not count[barrier][tag]:
+                    count[barrier][tag][data[key]["people_count"][0]["id"]] = []
+                    count[barrier][tag][data[key]["people_count"][1]["id"]] = []
+                    times[barrier][tag] = []
+                    dates[barrier][tag] = []
+                times[barrier][tag].append(data[key]["timestamp"])
+                dates[barrier][tag].append(datetime.datetime.fromtimestamp(int(data[key]["timestamp"])).strftime('%H:%M:%S'))
+                if requires_fit:
+                    count[barrier][tag][data[key]["people_count"][0]["id"]].append(t(data[key]["people_count"][0]["count"], prob_in))
+                    count[barrier][tag][data[key]["people_count"][1]["id"]].append(t(data[key]["people_count"][1]["count"], prob_out))
+                else:
+                    count[barrier][tag][data[key]["people_count"][0]["id"]].append(data[key]["people_count"][0]["count"])
+                    count[barrier][tag][data[key]["people_count"][1]["id"]].append(data[key]["people_count"][1]["count"])
+    
+        for loc in count[barrier][tag]:
+            cumulate[barrier][tag][loc] = np.cumsum(count[barrier][tag][loc])
+            if cumulate[barrier][tag][loc][-1] > 0:
+                scaling_factor[barrier][tag][loc]=gt[tag][loc]/cumulate[barrier][tag][loc][-1]
             else:
-                count[tag][data[key]["people_count"][0]["id"]].append(data[key]["people_count"][0]["count"])
-                count[tag][data[key]["people_count"][1]["id"]].append(data[key]["people_count"][1]["count"])
-
-    for loc in count[tag]:
-        cumulate[tag][loc] = np.cumsum(count[tag][loc])
-
-print("%20s : %-13s %5s %5s %4s%%" % ('time','location', 'track', 'gt', 'err'))
-for tag in tags:
-    a=[print("%20s : %-13s %5d %5d %4d%%" % (tag, loc, cumulate[tag][loc][-1], gt[tag][loc], int((cumulate[tag][loc][-1] - gt[tag][loc])/gt[tag][loc]*100))) for loc in cumulate[tag]]
+                scaling_factor[barrier][tag][loc]=1
+for barrier in barriers:
+    accumulated_error=0
+    print()
+    print(barrier, get_sf(scaling_factor[barrier])[id_in], get_sf(scaling_factor[barrier])[id_out])
+    #print("%20s : %-13s %5s %5s %5s %4s%%" % ('time','location', 'track', 'fit', 'gt', 'err'))
+    print("%20s : %-13s %5s %5s %4s%%" % ('time','location', 'track', 'gt', 'err'))
+    for tag in tags:
+        fitted[id_in]=cumulate[barrier][tag][id_in][-1]*get_sf(scaling_factor[barrier])[id_in]
+        fitted[id_out]=cumulate[barrier][tag][id_out][-1]*get_sf(scaling_factor[barrier])[id_out]
+        #print("%20s : %-13s %5d %5d %5d %4d%%" % (tag, id_in, cumulate[barrier][tag][id_in][-1], fitted[id_in], gt[tag][id_in], int((fitted[id_in] - gt[tag][id_in])/gt[tag][id_in]*100)))
+        print("%20s : %-13s %5d %5d %4d%%" % (tag, id_in, cumulate[barrier][tag][id_in][-1], gt[tag][id_in], int((fitted[id_in] - gt[tag][id_in])/gt[tag][id_in]*100)))
+        accumulated_error += abs(int((fitted[id_in] - gt[tag][id_in])/gt[tag][id_in]*100))
+        #print("%20s : %-13s %5d %5d %5d %4d%%" % (tag, id_out, cumulate[barrier][tag][id_out][-1], fitted[id_out], gt[tag][id_out], int((fitted[id_out] - gt[tag][id_out])/gt[tag][id_out]*100)))
+        print("%20s : %-13s %5d %5d %4d%%" % (tag, id_out, cumulate[barrier][tag][id_out][-1], gt[tag][id_out], int((fitted[id_out] - gt[tag][id_out])/gt[tag][id_out]*100)))
+        accumulated_error += abs(int((fitted[id_in] - gt[tag][id_in])/gt[tag][id_in]*100))
+    print('Accumulated absolute percentage error: ', accumulated_error,'%')
 
 
 #%%
