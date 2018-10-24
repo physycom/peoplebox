35380b05f2e2ea60fbc531dae7c13a764f2bbfc3
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-10-24 18:29:17 +0200

    add darkboat

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-rm -f save_stream save_frame_* json_triplet_packer darktrack_jetson* darknet.exe darknet_lib.* libdarknet_lib.* dump_* darknet/darknet darknet/libdarknet_lib.dylib darktest_track_on_video darkcrowd_*
+rm -f save_stream save_frame_* json_triplet_packer darkboat_ID* darktrack_jetson* darknet.exe darknet_lib.* libdarknet_lib.* dump_* darknet/darknet darknet/libdarknet_lib.dylib darktest_track_on_video darkcrowd_*
 echo "Removed binaries from install folders!"
