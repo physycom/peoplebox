cb979bde83e2f27962f5013955ad3d41c1d260ab
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-12-07 13:23:38 +0100

    update refs

--- build.ps1
+++ build.ps1
@@ -4,10 +4,7 @@
 New-Item -Path .\build_win -ItemType directory -Force
 Set-Location build_win
 
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DENABLE_CUDA:BOOL=TRUE" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" ..
+cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DENABLE_CUDA:BOOL=TRUE" "-DVCPKG_TARGET_TRIPLET=x64-windows" ..
 cmake --build . --target install --config Release
-
-#cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" "-DCMAKE_BUILD_TYPE=Release" -DENABLE_CUDA:BOOL=TRUE ..
-#cmake --build . --target install
 
 Set-Location ..
