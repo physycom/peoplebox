42b68bf82a11ebd4e7fde1681cb98b7223ec904f
allefabbri <rommacc@yahoo.it>
2019-03-21 10:57:48 +0100

    update submodule

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  build_dir=build_macos
+else
+  build_dir=build_lin
+fi
+
+
 if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
   export CC="/usr/local/bin/gcc-8"
   export CXX="/usr/local/bin/g++-8"
 fi
 
-./clean.sh
-mkdir -p build
-cd build
+mkdir -p $build_dir
+cd $build_dir
 cmake ..
 cmake --build . --target install
 cd ..
