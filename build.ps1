ce785d9ddf070dc929236da61f67e00c628034c5
allefabbri <rommacc@yahoo.it>
2019-03-21 09:33:43 +0100

    update submodule

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 #!/usr/bin/env powershell
 
 #Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
-New-Item -Path .\build -ItemType directory -Force
-Set-Location build
+New-Item -Path .\build_win -ItemType directory -Force
+Set-Location build_win
 
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" "-DCMAKE_BUILD_TYPE=Release" -DENABLE_ZED_CAMERA=FALSE ..
+cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --config Release --target install
 Set-Location ..
