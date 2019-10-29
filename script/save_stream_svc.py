672452f7d5182ec4baed3fd461e170a470422b3f
Alex <baroncini.alex@gmail.com>
2019-10-29 16:57:16 +0100

    added video to service

--- script/save_stream_svc.py
+++ script/save_stream_svc.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
  
+import cv2
 import sys
 import win32service
 import win32serviceutil
@@ -9,7 +10,7 @@ import time
 import json
 import threading
  
-CONFIGFILE = './service_config.json'
+CONFIGFILE = 'C:/Users/alex/Desktop/scripts/peoplebox/script/config.json'
  
 class Service(win32serviceutil.ServiceFramework):
  
@@ -31,28 +32,29 @@ class Service(win32serviceutil.ServiceFramework):
  
       try:
         with open(CONFIGFILE) as f:
-          cfg = json.load(f)
+          self.cfg = json.load(f)
       except Exception as e:
         self.log.write('{} : problems with config file {}\n'.format(datetime.now(), CONFIGFILE)); self.log.flush()
         self.log.write('{} : Exception -> {}\n'.format(datetime.now(), e)); self.log.flush()
         sleep = 5
         self.log.write('{} : sleeping for {}s\n'.format(datetime.now(), sleep)); self.log.flush()
-        #time.sleep(sleep)
+        time.sleep(sleep)
         exit(1)
- 
-      self.dt = int(cfg["chunk_len"]) # minutes
-      self.start = datetime.strptime(cfg["start_date"], "%Y-%m-%d %H:%M:%S")
-      self.stop = datetime.strptime(cfg["stop_date"], "%Y-%m-%d %H:%M:%S")
+      
+      # time schedule initialization
+      self.dt = int(self.cfg["chunk_len"]) # minutes
+      self.start = datetime.strptime(self.cfg["start_date"], "%Y-%m-%d %H:%M:%S")
+      self.stop = datetime.strptime(self.cfg["stop_date"], "%Y-%m-%d %H:%M:%S")
       self.pause = []
-      for p in cfg['pause_interval']:
+      for p in self.cfg['pause_interval']:
         [p0,p1] = p.split('-')
         self.pause.append((datetime.strptime(p0, "%H:%M"), datetime.strptime(p1, "%H:%M")))
       starth = datetime.strptime(self.start.strftime('%Y-%m-%d %H'), '%Y-%m-%d %H')
-      minutes = (int(self.start.strftime("%M"))*self.dt)//self.dt + self.dt
+      minutes = (int(self.start.strftime("%M"))*self.dt)//self.dt
       start0 = (starth + timedelta(minutes=minutes)).strftime("%Y-%m-%d %H:%M:%S")
       ti = datetime.strptime(start0, "%Y-%m-%d %H:%M:%S")
       self.schedule = []
-      while ti <= self.stop:
+      while ti < self.stop:
         skip = False
         for p in self.pause:
           ti_t, p0_t, p1_t = ti.time(), p[0].time(), p[1].time()
@@ -72,47 +74,98 @@ class Service(win32serviceutil.ServiceFramework):
  
       with open(self.wdir + '/schedule.json', 'w') as f:
         json.dump(self.schedule, f, indent=2)
+        
+      # cam modules initialization
+      self.cams = []
+      self.urls = []
+      for i,c in enumerate([ c for c in sorted(self.cfg.keys()) if 'cam' in c ]):
+        self.urls.append(self.cfg[c]['url'])
+        self.cams.append(cv2.VideoCapture(self.urls[i]))
+      
     except Exception as e:
       with open(self.wdir + '/debug.log', 'w') as f:
-        f.write('Error : {}'.format(e))
+        f.write('Init Error : {}'.format(e))
         f.close()
  
  
   def SvcDoRun(self):
- 
-    rc = None
-    self.log.write('{} : {} STARTING UP\n'.format(datetime.now())), self._svc_description_)); self.log.flush()
- 
-    si = 0
-    tnow = datetime.now()
-    while rc != win32event.WAIT_OBJECT_0 and tnow < self.stop:
- 
-#      if tnow < schedule[si]['start_t']:
-#        continue
-#      else:
-#        for i,c in enumerate([ c for c in sorted(cfg.keys()) if 'cam' in c ]):
-#          threading.Thread(target=self.do_thread,args=[self.log, i, schedule[si]]).start()
-#        si += 1
-#        tnow = datetime.now()
- 
-      rc = win32event.WaitForSingleObject(self.hWaitStop, 999)
- 
-  def do_thread(self, i, s):
-    self.log.write('{} : THREAD {} acquiring {}'.format(datetime.now(), i, s['start_t'])); self.log.flush()
-    # opencv stuff
-    # with some error handling
-    time.sleep(20)
-    # sleep to fake lenghty operation
-    self.log.write('{} : THREAD {} acquisition done'.format(datetime.now(), i)); self.log.flush()
- 
+    try:
+      rc = None
+      self.log.write('{} : {} STARTING UP\n'.format(datetime.now(), self._svc_description_)); self.log.flush()
+      si = 0
+      tnow = datetime.now()
+      
+      while rc != win32event.WAIT_OBJECT_0 and tnow < self.stop and si < len(self.schedule):
+        if tnow >= datetime.strptime(self.schedule[si]['start_t'], "%Y-%m-%d %H:%M:%S"):
+          for i,c in enumerate([ c for c in sorted(self.cfg.keys()) if 'cam' in c ]):
+            threading.Thread(target=self.do_thread,args=(i, self.schedule[si])).start()
+          si += 1
+        tnow = datetime.now()
+        rc = win32event.WaitForSingleObject(self.hWaitStop, 999)
+        
+      time.sleep(self.dt*60 + 5)
+      self.SvcStop()
+      
+    except Exception as e:
+      with open(self.wdir + '/debug.log', 'w') as f:
+        f.write('Run Error : {}'.format(e))
+        f.close()
  
   def SvcStop(self):
- 
-    self.time = datetime.now()
-    self.log.write('{} : SERVICE SHUTDOWN\n'.format(self.time)); self.log.flush()
+    self.log.write('{} : SERVICE SHUTDOWN\n'.format(datetime.now())); self.log.flush()
     self.log.close()
+    for c in self.cams:
+      c.release()
     self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
     win32event.SetEvent(self.hWaitStop)
+    
+  def do_thread(self, i, s):
+    try:
+      tnow = datetime.now()
+      self.log.write('{} : THREAD {} acquiring {}\n'.format(tnow, i, s['start_t'])); self.log.flush()
+      
+      while tnow < datetime.strptime(s['stop_t'], "%Y-%m-%d %H:%M:%S"):
+        try:
+          self.cam_record(i, s)
+          break
+        except ValueError as v:
+          tnow = datetime.now()
+          self.log.write('{} error: {}, time {}\n'.format(i,v, tnow)); self.log.flush()
+          continue
+      self.log.write('{} : THREAD {} acquisition done\n'.format(datetime.now(), i)); self.log.flush()
+      
+    except Exception as e:
+      with open(self.wdir + '/debug.log', 'w') as f:
+        f.write('Thread Error : {}'.format(e))
+        f.close()
+      
+ 
+  def cam_record(self, i, s):
+    camop = self.cams[i].grab()
+    if camop:
+      tcap = datetime.now()
+      writer = cv2.VideoWriter(
+              self.wdir + '/video_{}_{}.mp4'.format(i, datetime.strftime(tcap,"%Y%m%d_%H%M")),
+              cv2.VideoWriter_fourcc(*'mp4v'),
+              self.cams[i].get(cv2.CAP_PROP_FPS),
+              (int(self.cams[i].get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cams[i].get(cv2.CAP_PROP_FRAME_HEIGHT))) 
+            )
+      if writer.isOpened():
+        while tcap < datetime.strptime(s['stop_t'], "%Y-%m-%d %H:%M:%S"):
+          ret, frame = self.cams[i].read()
+          if ret:
+              writer.write(frame)
+              tcap = datetime.now()
+          else:
+              writer.release()
+              raise ValueError('Cam {} not capturing'.format(i))
+        writer.release()
+      else:
+        raise ValueError('Video writer {} not opened'.format(i))
+    else:
+      self.cams[i].open(self.urls[i])
+      raise ValueError('Cam {} not opened'.format(i))
+    return
  
 if __name__ == '__main__':
   win32serviceutil.HandleCommandLine(Service)
