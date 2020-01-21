690ff2432b32c470e6663dbe9fb544f65c8beb8a
Alex <baroncini.alex@gmail.com>
2020-01-21 15:07:21 +0100

    darknet un-submod part3 - the finale. now works with vcpkg darknet

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ $number_of_build_workers = 12
 New-Item -Path .\build_win -ItemType directory -Force
 Set-Location build_win
 
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" "-DCMAKE_BUILD_TYPE=Release" "-DDarknet_DIR=.\darknet\share\darknet" ..
+cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=$env:VCPKG_DEFAULT_TRIPLET" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --config Release --parallel ${number_of_build_workers} --target install
 #Copy-Item *.dll ..
 Set-Location ..
